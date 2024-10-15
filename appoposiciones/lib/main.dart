bdc155e432b2e2ed7ebd483d6fd4ea24bdbbd965
AChacon97 <acg0077@alu.medac.es>
2024-10-15 21:35:27 +0200

    cambios repositorio

--- appoposiciones/lib/main.dart
+++ appoposiciones/lib/main.dart
@@ -1,144 +1,162 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:appoposiciones/registro.dart'; // Importa la pantalla de registro
+import 'package:flutter/material.dart'; // Importa el paquete de Material Design
+import 'package:firebase_core/firebase_core.dart'; // Importa el paquete para Firebase
+import 'firebase_options.dart'; // Importa la configuración de Firebase
 
+// Función principal que se ejecuta al iniciar la aplicación
 void main() async {
+  // Inicializa Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Ejecuta la aplicación
   runApp(const MyApp());
 }
 
+// Clase principal de la aplicación
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor de la clase
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', // Título de la aplicación
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple), // Tema de la aplicación
+        useMaterial3: true, // Usar el diseño Material 3
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:
+          const MyHomePage(title: 'Flutter Demo Home Page'), // Página principal
     );
   }
 }
 
+// Clase para la página principal
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage(
+      {super.key, required this.title}); // Constructor con el título
 
-  final String title;
+  final String title; // Variable para almacenar el título
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState(); // Estado de la página
 }
 
+// Clase para el estado de MyHomePage
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('PROYECTO'),
+      appBar: AppBar(
+        backgroundColor:
+            Theme.of(context).colorScheme.inversePrimary, // Color de la AppBar
+        title: const Text('PROYECTO'), // Título de la AppBar
+      ),
+      body: Padding(
+        padding:
+            const EdgeInsets.all(20.0), // Espaciado alrededor del contenido
+        child: Column(
+          mainAxisSize: MainAxisSize.max, // Ocupa el máximo espacio posible
+          children: <Widget>[
+            // Imagen de la autoescuela
+            Image.asset(
+              "assets/images/Imagen de WhatsApp 2024-09-29 a las 14.01.40_25073ee1 - copia.jpg",
+              height: 100, // Altura de la imagen
+              width: 150, // Ancho de la imagen
+            ),
+            const Text(
+              'BIENVENIDOS A TU APP DE AUTOESCUELA', // Texto de bienvenida
+              style: TextStyle(
+                fontSize: 28.0, // Tamaño de la letra
+                fontWeight: FontWeight.bold, // La letra en negrita
+                color: Colors.blueAccent, // Color de la letra
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(5.0, 10.0), // Desplazamiento de la sombra
+                    blurRadius: 3.0, // Difuminado de la sombra
+                    color: Colors.black26, // Color de la sombra
+                  )
+                ],
+                letterSpacing: 2.0, // Espaciado entre letras
+              ),
+            ),
+            const SizedBox(height: 30), // Espacio vertical entre widgets
+
+            // Campo de texto para el usuario
+            Container(
+              width: 475, // Ancho del contenedor
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.white, // Color del texto de sugerencia
+                    fontSize: 20.0, // Tamaño del texto de sugerencia
+                    fontWeight: FontWeight.bold, // Negrita
+                  ),
+                  hintText: 'Usuario', // Texto de sugerencia
+                  fillColor: Colors.red, // Color de fondo del campo
+                  filled: true, // Habilitar el relleno
+                ),
+              ),
+            ),
+            const SizedBox(height: 30), // Espacio vertical entre widgets
+
+            // Campo de texto para la contraseña
+            Container(
+              width: 475, // Ancho del contenedor
+              child: const TextField(
+                obscureText: true, // Oculta el texto ingresado
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(
+                    color: Colors.white, // Color del texto de sugerencia
+                    fontSize: 20.0, // Tamaño del texto de sugerencia
+                    fontWeight: FontWeight.bold, // Negrita
+                  ),
+                  hintText: 'Contraseña', // Texto de sugerencia
+                  fillColor: Colors.red, // Color de fondo del campo
+                  filled: true, // Habilitar el relleno
+                ),
+              ),
+            ),
+            const SizedBox(height: 25), // Espacio vertical entre widgets
+
+            // Fila para los botones
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Centrar los botones
+              children: <Widget>[
+                BotonRegistrar(context), // Botón de registro
+                const SizedBox(width: 15), // Espacio horizontal entre botones
+                BotonAcceder(), // Botón de acceder
+              ],
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Image.asset(
-                "assets/images/Imagen de WhatsApp 2024-09-29 a las 14.01.40_25073ee1 - copia.jpg",
-                height: 100,
-                width: 150,
-              ),
-              const Text(
-                'BIENVENIDOS A TU APP DE AUTOESCUELA',
-                style: TextStyle(
-                  fontSize: 28.0, // Tamaño de la letra.
-                  fontWeight: FontWeight.bold, // La letra en negrita.
-                  color: Colors.blueAccent, // Color de la letra
-                  shadows: <Shadow>[
-                    Shadow(
-                      // Permite añadir sombras a los textos
-                      offset: Offset(5.0, 10.0), // Desplazamiento de la sombra.
-                      blurRadius: 3.0, // Difuminado de la sombra.
-                      color: Colors.black26, // Color de la sombra
-                    )
-                  ],
-                  letterSpacing: 2.0,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                // Esto es un contenedor donde recogemos el TextField de la barra, se ha creado para poder meter un width para el ancho
-                width: 475,
-                child: const TextField(
-                  // Todo lo que conlleva la barra usuario.
-                  decoration: InputDecoration(
-                    hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
-                    hintText: 'Usuario',
-                    fillColor: Colors.red,
-                    filled: true,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                //Esto es un contenedor donde recogemos el TextField de la barra, se ha creado para poder meter un width para el ancho.
-                width: 475,
-                child: const TextField(
-                  // Todo lo que conlleva la barra contraseña.
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintStyle: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
-                    hintText: 'Contraseña',
-                    fillColor: Colors.red,
-                    filled: true,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                  //fila para acoger los métodos de los botones.
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    BotonRegistrar(),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    BotonAcceder(),
-                  ]),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
 
-Widget BotonRegistrar() {
-  // Método para el botón registrar
+// Widget para el botón de registrar
+Widget BotonRegistrar(BuildContext context) {
   return ElevatedButton(
-    onPressed: () {},
-    child: const Text('Registrar'),
+    onPressed: () {
+      // Navegar a la pantalla de registro al presionar el botón
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const PantallaRegistro()),
+      );
+    },
+    child: const Text('Registrar'), // Texto del botón
   );
 }
 
+// Widget para el botón de acceder
 Widget BotonAcceder() {
-  // Método para el botón acceder.
   return ElevatedButton(
-    onPressed: () {},
-    child: const Text('Acceder'),
+    onPressed: () {
+      // Aquí iría la lógica para acceder al usuario
+    },
+    child: const Text('Acceder'), // Texto del botón
   );
 }
--- appoposiciones/lib/registro.dart
+++ appoposiciones/lib/registro.dart
@@ -1,10 +1,130 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+// Clase principal para la pantalla de registro
+class PantallaRegistro extends StatelessWidget {
+  const PantallaRegistro({super.key}); // Constructor de la clase
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro'), // Título de la barra de aplicación
+      ),
+      body: Center(
+        // Centra todo el contenido en la pantalla
+        child: Padding(
+          padding:
+              const EdgeInsets.all(20.0), // Espaciado alrededor del contenido
+          child: Column(
+            mainAxisSize:
+                MainAxisSize.min, // Minimiza el tamaño vertical del Column
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centra el contenido verticalmente
+            children: <Widget>[
+              // Se eliminó el SizedBox en la parte superior
+
+              const Text(
+                'Regístrate', // Texto de encabezado
+                style: TextStyle(
+                  fontSize: 28.0, // Tamaño de la letra
+                  fontWeight: FontWeight.bold, // Estilo en negrita
+                  color: Colors.blueAccent, // Color del texto
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(5.0, 10.0), // Desplazamiento de la sombra
+                      blurRadius: 3.0, // Difuminado de la sombra
+                      color: Colors.black26, // Color de la sombra
+                    )
+                  ],
+                  letterSpacing: 2.0, // Espaciado entre letras
+                ),
+              ),
+              const SizedBox(height: 30), // Espacio entre widgets
+
+              // Contenedor para el campo de nombre de usuario
+              Container(
+                width: 475, // Ancho del contenedor
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      color: Colors.white, // Color del texto de sugerencia
+                      fontSize: 20.0, // Tamaño del texto de sugerencia
+                      fontWeight: FontWeight.bold, // Negrita
+                    ),
+                    hintText: 'Nombre de Usuario', // Texto de sugerencia
+                    fillColor: Colors.red, // Color de fondo del campo
+                    filled: true, // Habilitar el relleno
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25), // Espacio entre widgets
+
+              // Contenedor para el campo de correo electrónico
+              Container(
+                width: 475, // Ancho del contenedor
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    hintText: 'Correo Electrónico', // Texto de sugerencia
+                    fillColor: Colors.red,
+                    filled: true,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25), // Espacio entre widgets
+
+              // Contenedor para el campo de teléfono
+              Container(
+                width: 475, // Ancho del contenedor
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    hintText: 'Teléfono', // Texto de sugerencia
+                    fillColor: Colors.red,
+                    filled: true,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25), // Espacio entre widgets
+
+              // Contenedor para el campo de contraseña
+              Container(
+                width: 475, // Ancho del contenedor
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    hintText: 'Contraseña', // Texto de sugerencia
+                    fillColor: Colors.red,
+                    filled: true,
+                  ),
+                  obscureText: true, // Oculta el texto ingresado
+                ),
+              ),
+              const SizedBox(height: 25), // Espacio entre widgets
+
+              // Botón para registrar al usuario
+              ElevatedButton(
+                onPressed: () {
+                  // Aquí iría la lógica para registrar al usuario.
+                },
+                child: const Text('Registrar'), // Texto del botón
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
