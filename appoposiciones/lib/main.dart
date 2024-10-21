7de315bde6638b9a96aa490629cf8c92c6d516a5
AChacon97 <acg0077@alu.medac.es>
2024-10-21 18:26:20 +0200

    registros

--- appoposiciones/lib/main.dart
+++ appoposiciones/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -38,12 +39,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  void initState() {      // Mostrar el diálogo automáticamente cuando la pantalla se inicia
+  void initState() {
+    // Mostrar el diálogo automáticamente cuando la pantalla se inicia
     super.initState();
-    
-    WidgetsBinding.instance.addPostFrameCallback((_) {  /* El WidgetsBinding es la clase que se encarga de la interacción entre el framework de flutter y el motor de renderizado.*/
-                                                                          // El addPostFrameCallBack((_) {...}) : Asegura que el código dentro del callback se ejecute justo después de que se haya completado el dibijo de la interfaz de usuario.
-                                                                          // El _showMyDialog(); : Dentro del callback, se llama a la función _showMyDialog(), que muestra dialogo.
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      /* El WidgetsBinding es la clase que se encarga de la interacción entre el framework de flutter y el motor de renderizado.*/
+      // El addPostFrameCallBack((_) {...}) : Asegura que el código dentro del callback se ejecute justo después de que se haya completado el dibijo de la interfaz de usuario.
+      // El _showMyDialog(); : Dentro del callback, se llama a la función _showMyDialog(), que muestra dialogo.
       _showMyDialog();
     });
   }
@@ -51,14 +54,16 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _showMyDialog() async {
     return showDialog<void>(
       context: context,
-      barrierDismissible: false, // El usuario no puede cerrar el diálogo tocando fuera
+      barrierDismissible:
+          false, // El usuario no puede cerrar el diálogo tocando fuera
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('Bienvenido'),
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text('Hola. Si no estás registrado, resgistrate. Si de lo contrario estás registrado, accede pulsando en login'),
+                Text(
+                    'Hola. Si no estás registrado, resgistrate. Si de lo contrario estás registrado, accede pulsando en login'),
               ],
             ),
           ),
@@ -66,15 +71,20 @@ class _MyHomePageState extends State<MyHomePage> {
             TextButton(
               child: Text('registro'),
               onPressed: () {
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context) =>PantallaRegistro()));// Cerrar el diálogo
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            PantallaRegistro())); // Cerrar el diálogo
               },
             ),
-             TextButton(
+            TextButton(
               child: Text('login'),
               onPressed: () {
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context) =>  PantallaLogin(title: 'login')));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PantallaLogin(title: 'login')));
               },
             ),
           ],
