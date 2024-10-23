3d473a41c0e948c001c3d3fda1138e54daa17a67
Luis Perez <lpi0006@alu.medac.es>
2024-10-23 17:31:03 +0200

    Commit

--- appoposiciones/lib/temario_test.dart
+++ appoposiciones/lib/temario_test.dart
@@ -1,12 +1,10 @@
-
-
 import 'package:appoposiciones/preguntas_test.dart';
 import 'package:flutter/material.dart';
 
 class Temario_Test extends StatelessWidget {
- // const Temario_Test({super.key});
+  // const Temario_Test({super.key});
   final int numeroDeTemas;
-   const Temario_Test({super.key, required this.numeroDeTemas});
+  const Temario_Test({super.key, required this.numeroDeTemas});
 //dfsdfds
   @override
   Widget build(BuildContext context) {
@@ -17,29 +15,32 @@ class Temario_Test extends StatelessWidget {
       ),
       backgroundColor: const Color.fromARGB(255, 1, 194, 248),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
+          child: SingleChildScrollView(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget> [
+          children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(padding: EdgeInsets.all(20.0),
-                child: Text('TEMAS',style: TextStyle(color: Colors.white, fontSize: 30.0),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'TEMAS',
+                    style: TextStyle(color: Colors.white, fontSize: 30.0),
                   ),
                 ),
               ],
             ),
-              for(int i =0; i<numeroDeTemas; i+=2)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _boton(context, 'Tema ${i+1}'),  // tema 1,3,5, etc.
-                    if (i + 1 < numeroDeTemas)
-                      _boton(context, 'Tema ${i+2}'), // Tema 2,4,6, etc.
-                  ],
-                )
-           /* Row(
+            for (int i = 0; i < numeroDeTemas; i += 2)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _boton(context, 'Tema ${i + 1}'), // tema 1,3,5, etc.
+                  if (i + 1 < numeroDeTemas)
+                    _boton(context, 'Tema ${i + 2}'), // Tema 2,4,6, etc.
+                ],
+              )
+            /* Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 _boton(context, 'Tema 1'),
@@ -76,36 +77,33 @@ class Temario_Test extends StatelessWidget {
             ),*/
           ],
         ),
-        )
-        
-      ),
+      )),
     );
   }
 }
 
-Widget _boton (BuildContext context, String nombre){
+Widget _boton(BuildContext context, String nombre) {
   return InkWell(
     child: Container(
-      width: 70, height: 70,                                // Anchura y altura de la carta
+      width: 70, height: 70, // Anchura y altura de la carta
       child: Card(
-        margin: EdgeInsets.all(5.0),                      // Margen entre las cartas
-        color: const Color.fromARGB(255, 2, 244, 10),  // Color de la carta
-        elevation: 10.0,                                // Elevamos el botón
-        shadowColor: Colors.red,                     // Color de la sombra
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(nombre),
-          ],
-        )
-      ),
+          margin: EdgeInsets.all(5.0), // Margen entre las cartas
+          color: const Color.fromARGB(255, 2, 244, 10), // Color de la carta
+          elevation: 10.0, // Elevamos el botón
+          shadowColor: Colors.red, // Color de la sombra
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(nombre),
+            ],
+          )),
     ),
     onTap: () {
       Navigator.push(
-        context, 
+        context,
         MaterialPageRoute(builder: (context) => Preguntas_Test(nombre: nombre)),
-        );
+      );
     },
   );
 }
