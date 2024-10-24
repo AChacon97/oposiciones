26a97780dce94c8a30d337b1275820598f8201e3
ant3796 <aanton3796@gmail.com>
2024-10-24 19:33:13 +0200

    flut

--- appoposiciones/ios/Flutter/flutter_export_environment.sh
+++ appoposiciones/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Jose Manuel\Desktop\github\oposiciones\appoposiciones"
+export "FLUTTER_ROOT=D:\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\dev\proyectos\oposiciones\appoposiciones"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
