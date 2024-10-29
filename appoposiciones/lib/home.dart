e8011a580c6b27e547b37ea01d5b0fd918ecea6e
XaruCat <deb0004@alu.medac.es>
2024-10-29 12:14:36 +0100

    Automatización Botonera Temas
    Aparecen tantos botones como temas haya en el Json.
    Corregido el texto de los botones, ahora aparece tanto Nº de tema como el título

--- appoposiciones/lib/home.dart
+++ appoposiciones/lib/home.dart
@@ -92,6 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
           : Center(
               child: temas.isEmpty
                   ? CircularProgressIndicator() // Muestra un indicador de carga mientras se cargan los temas
+// Dentro de `build` y específicamente en el `ListView` donde se generan los botones:
                   : ListView(
                       padding: EdgeInsets.all(16.0),
                       children: temas.map((tema) {
@@ -118,11 +119,45 @@ class _HomeScreenState extends State<HomeScreen> {
                                 _updateTemaProgress(tema['tema'], 1);
                               }
                             },
-                            child: Text(tema['nombre_tema']),
+                            // Actualiza el `Text` del botón para mostrar el prefijo "Tema ", el número, y el título.
+                            child: Text(
+                                "Tema ${tema['tema']}. ${tema['nombre_tema']}"),
                           ),
                         );
                       }).toList(),
                     ),
+
+              // : ListView(
+              //     padding: EdgeInsets.all(16.0),
+              //     children: temas.map((tema) {
+              //       return Padding(
+              //         padding: const EdgeInsets.symmetric(vertical: 8.0),
+              //         child: ElevatedButton(
+              //           style: _getButtonStyle(tema['tema']),
+              //           onPressed: () async {
+              //             _updateTemaProgress(tema['tema'], 1);
+
+              //             final resultado = await Navigator.push(
+              //               context,
+              //               MaterialPageRoute(
+              //                 builder: (context) => _currentIndex == 0
+              //                     ? Preguntas_Test(nombre: tema['tema'])
+              //                     : Preguntas_Desarrollo(
+              //                         nombre: tema['tema']),
+              //               ),
+              //             );
+
+              //             if (resultado == 'completado') {
+              //               _updateTemaProgress(tema['tema'], 2);
+              //             } else {
+              //               _updateTemaProgress(tema['tema'], 1);
+              //             }
+              //           },
+              //           child: Text(tema['nombre_tema']),
+              //         ),
+              //       );
+              //     }).toList(),
+              //   ),
             ),
       bottomNavigationBar: BottomNavigationBar(
         iconSize: 60,
